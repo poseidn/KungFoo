0b6021ea307b8ffe8c2bf84d8411d0091ececec1
poseidn <poseidon@saturn>
2016-01-12 17:11:49 +0100

    compiling native code for all available platforms now

--- build_android/jni/Application.mk
+++ build_android/jni/Application.mk
@@ -3,8 +3,8 @@ NDK_TOOLCHAIN_VERSION := 4.8
 
 ## compile for all available platforms, use this for release
 ## with all binaries is the apk only around 4 MB larger
-##APP_ABI := all
+APP_ABI := all
 # best arm compatibility possible
-APP_ABI := armeabi
+#APP_ABI := armeabi
 ##APP_STL := gnustl_shared
 APP_STL := gnustl_static
